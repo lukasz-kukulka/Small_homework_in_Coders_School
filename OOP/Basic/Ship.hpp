9fc6ce9c62e4dc907a4012ea4a9a488ab6a2bb4c
lukasz-kukulka <qqla83@gmail.com>
2021-06-13 13:56:49 +0100

    Add getters for Ship class

--- OOP/Basic/Ship.hpp
+++ OOP/Basic/Ship.hpp
@@ -1,13 +1,20 @@
 #pragma once
 
+#include <string>
 class Ship {
 
 public:
-
-
-
+    uint8_t getId() const { return id_; };
+    std::string getName() const& { return name_; };
+    uint8_t getSpeed() const { return speed_; };
+    uint8_t getMaxCrew() const { return maxCrew_; };
+    uint8_t getCapacity() const { return capacity_; };
 private:
-
+    uint8_t id_ { };
+    std::string name_ { };
+    uint8_t speed_ { 50 };
+    const uint8_t maxCrew_ { 100 };
+    const uint8_t capacity_ { 80 };
 };
 
 
