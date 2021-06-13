d28c8be09d8a5b3a7804cb1e6379810c93f4c268
lukasz-kukulka <qqla83@gmail.com>
2021-06-13 14:27:38 +0100

    Add ship name setter

--- OOP/Basic/Ship.cpp
+++ OOP/Basic/Ship.cpp
@@ -11,3 +11,7 @@ Ship::Ship(int8_t id, std::string name, uint8_t speed, uint8_t maxCrew, uint8_t 
 Ship::Ship(int8_t id, uint8_t speed, uint8_t maxCrew) 
     : Ship(id, "Ship", speed, maxCrew, 100)
 {}
+
+void Ship::setName(const std::string& name){
+    name_ = name;
+}
