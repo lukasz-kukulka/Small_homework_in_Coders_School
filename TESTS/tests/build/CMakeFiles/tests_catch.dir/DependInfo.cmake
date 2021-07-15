71480adac429eaa2f7939f5d298ece78ce213f26
lukasz-kukulka <qqla83@gmail.com>
2021-07-15 18:41:55 +0100

    Add cmakefile to tests

---
+++ TESTS/tests/build/CMakeFiles/tests_catch.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/nanautzin/coders_school/Small_homework_in_Coders_School/TESTS/tests/catch_amalgamated.cpp" "/home/nanautzin/coders_school/Small_homework_in_Coders_School/TESTS/tests/build/CMakeFiles/tests_catch.dir/catch_amalgamated.cpp.o"
+  "/home/nanautzin/coders_school/Small_homework_in_Coders_School/TESTS/tests/main_catch.cpp" "/home/nanautzin/coders_school/Small_homework_in_Coders_School/TESTS/tests/build/CMakeFiles/tests_catch.dir/main_catch.cpp.o"
+  "/home/nanautzin/coders_school/Small_homework_in_Coders_School/TESTS/tests/test.cpp" "/home/nanautzin/coders_school/Small_homework_in_Coders_School/TESTS/tests/build/CMakeFiles/tests_catch.dir/test.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ TESTS/tests/build/CMakeFiles/tests_catch.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tests_catch.dir/catch_amalgamated.cpp.o"
+  "CMakeFiles/tests_catch.dir/main_catch.cpp.o"
+  "CMakeFiles/tests_catch.dir/test.cpp.o"
+  "tests_catch"
+  "tests_catch.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tests_catch.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
