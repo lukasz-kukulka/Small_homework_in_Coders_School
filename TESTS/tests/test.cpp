ab05896a906993e70538e77a66c92491302203be
lukasz-kukulka <qqla83@gmail.com>
2021-07-15 16:39:47 +0100

    testing catch2 std::sort firt version

--- TESTS/tests/test.cpp
+++ TESTS/tests/test.cpp
@@ -1,13 +1,38 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch_amalgamated.hpp"
 
-unsigned int Factorial( unsigned int number ) {
-    return number <= 1 ? number : Factorial(number-1)*number;
-}
+#include <algorithm>
+#include <vector>
 
-TEST_CASE( "Factorials are computed", "[factorial]" ) {
-    REQUIRE( Factorial(1) == 1 );
-    REQUIRE( Factorial(2) == 2 );
-    REQUIRE( Factorial(3) == 6 );
-    REQUIRE( Factorial(10) == 3628800 );
+SCENARIO("sort algotithm can be initialize in different way", "std::sort") {
+    GIVEN("A vector with some items") {
+        std::vector<int> v = {3, 1, 4, 2, 5};
+        
+
+        WHEN("sort in base way") {
+            std::sort(v.begin(), v.end());
+
+            THEN("check single value in vector") {
+                REQUIRE(v[0] == 1);
+                REQUIRE(v[1] == 2);
+                REQUIRE(v[2] == 3);
+                REQUIRE(v[3] == 4);
+                REQUIRE(v[4] == 5);
+                REQUIRE(std::is_sorted(begin(v), end(v)) == true);
+            }
+        }
+
+        WHEN("sort in reverse way") {
+            std::sort(v.begin(), v.end(), [](int a, int b){ return a > b; });
+
+            THEN("check single value in vector") {
+                REQUIRE(v[0] == 5);
+                REQUIRE(v[1] == 4);
+                REQUIRE(v[2] == 3);
+                REQUIRE(v[3] == 2);
+                REQUIRE(v[4] == 1);
+                REQUIRE(std::is_sorted(rbegin(v), rend(v)) == true);
+            }
+        }
+    }
 }
