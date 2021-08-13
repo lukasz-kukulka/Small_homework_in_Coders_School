39e3ee4ba8ee3bc8ba394047f5720914b1f268e9
lukasz-kukulka <qqla83@gmail.com>
2021-08-13 01:44:30 +0100

    Task with map, two versions

--- STL/associativeContainers.cpp
+++ STL/associativeContainers.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
 #include <map>
+#include <string>
+#include <cmath>
+
+float distanceToZeroPoint(int x, int y) {
+    return std::sqrt(std::abs(x) * std::abs(x) + std::abs(y) * std::abs(y));
+}
 
 int main() {
     std::multimap<int, std::string> map;
@@ -21,5 +27,45 @@ int main() {
             std::cout << k << " | " << v << '\n';
         }
     }
+    std::cout << "_________________________________________________________________________________________" << '\n';
+
+    struct Point {
+        int y;
+        int x;
+
+        bool operator<(const Point point) const {
+            return (std::sqrt(std::abs(point.x) * std::abs(point.x) + std::abs(point.y) * std::abs(point.y))) < 
+                   (std::sqrt(std::abs(x) * std::abs(x) + std::abs(y) * std::abs(y)));
+        }
+    };
+
+
+
+    std::map<std::string, Point> map2{ { "Wrocław", {17, 51}},
+                                       { "Moskwa", {37, 55} },
+                                       { "Nowy Jork", {-74, 40} },
+                                       { "Sydney", {151, -33} }, };
+    for (const auto& [k, v] : map2) {
+        if (distanceToZeroPoint(v.x, v.y) == 70) {
+            std::cout << "Distance 70 have " << k << '\n';
+        }
+    }
+    auto searchEle = map2.find("Sydney");
+    std::cout << searchEle->first << " distance to point 0 = " << distanceToZeroPoint(searchEle->second.x, searchEle->second.y) << '\n';
+    std::cout << "_________________________________________________________________________________________" << '\n';
+
+    std::map<Point, std::string> map3{ { {17, 51}, "Wrocław" },
+                                       { {37, 55}, "Moskwa" },
+                                       { {-74, 40}, "Nowy Jork" },
+                                       { {151, -33}, "Sydney" }, };
+    for (const auto& [k, v] : map3) {
+        if (distanceToZeroPoint(k.x, k.y) == 70) {
+            std::cout << "Distance 70 have " << v << '\n';
+        }
+        if (v == "Sydney") {
+            std::cout << "Sydney coordinates = (" << k.y << " " << k.x << ")\n";
+        }
+    }
+
     return 0;
 }
